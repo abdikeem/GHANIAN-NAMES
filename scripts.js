16a4ac94c0b004daec07f137ff1dc248d3d645c4
Abdihakim <abdihakimabdirahman21@gmail.com>
2019-09-21 13:56:00 +0300

    add README.md

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
-document.getElementById('button').addEventLIsten ('click', function() {
-    document.querySelector('wallece').style.display = 'flex', })
-
-    document.querySelector('close').addEventListener('click', function() {
-        document.querySelector ('wallece').style.display ='none'});
+// 
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
 }
