f208d036ff0af9ac8640f67f57cecb90d572dd06
Abdihakim <abdihakimabdirahman21@gmail.com>
2019-09-21 12:09:33 +0300

    extended th econtent

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('button').addEventLIsten ('click', function() {
+    document.querySelector('wallece').style.display = 'flex', })
+
+    document.querySelector('close').addEventListener('click', function() {
+        document.querySelector ('wallece').style.display ='none'});
+}
